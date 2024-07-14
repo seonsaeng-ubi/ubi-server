a844e2458220a32043c7ba144088a29ba707714b
no-gari <sofaissofa@icloud.com>
2024-07-15 00:28:26 +0900

    add all features

--- api/problem/urls.py
+++ api/problem/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SubjectAPIView, RegionAPIView, PracticeProblemListAPIVIew, RandomPracticeProblemListAPIVIew, \
-    RealProblemSetAPIView, RealProblemListAPIVIew, ScrapUpdateAPIView, ScrappedProblemListAPIView, ProblemDetailAPIView
+    RealProblemSetAPIView, RealProblemListAPIVIew, ScrapUpdateAPIView, ScrappedProblemListAPIView, \
+    ProblemDetailAPIView, AllRandomPracticeProblemListAPIVIew, AllPracticeProblemListAPIVIew, \
+    AllRealProblemListAPIVIew, AllScrappedProblemListAPIView
 
 urlpatterns = [
     path('regions/', RegionAPIView.as_view()),
@@ -8,12 +10,19 @@ urlpatterns = [
     # 연습 문제 리스트
     path('practice-questions/', PracticeProblemListAPIVIew.as_view()),
     path('practice-questions/random/', RandomPracticeProblemListAPIVIew.as_view()),
+    # 연습 문제 리스트, 페이지네이션 제외
+    path('practice-questions/all/', AllPracticeProblemListAPIVIew.as_view()),
+    path('practice-questions/random/all/', AllRandomPracticeProblemListAPIVIew.as_view()),
     # 실전 문제 세트
     path('real-questions-set/', RealProblemSetAPIView.as_view()),
     # 실전 문제 리스트
     path('real-questions/', RealProblemListAPIVIew.as_view()),
+    # 실전 문제 리스트, 페이지네이션 제외
+    path('real-questions/all/', AllRealProblemListAPIVIew.as_view()),
     # 오답노트 문제 리스트
     path('scrapped-questions/', ScrappedProblemListAPIView.as_view()),
+    # 오답노트 문제 리스트, 페이지네이션 제외
+    path('scrapped-questions/all/', AllScrappedProblemListAPIView.as_view()),
     # 스크랩 / 해제
     path('update/<int:pk>/', ScrapUpdateAPIView.as_view()),
     # 문제 상세
--- api/problem/views.py
+++ api/problem/views.py
@@ -1,10 +1,9 @@
 from .serializers import BigSubjectSerializer, RegionSerializer, RealProblemSetSerializer, \
     ProblemListSerializer, ProblemUpdateSerializer
-from .models import BigSubject, Region, ProblemSet, Problem, SmallSubject
+from .models import BigSubject, Region, ProblemSet, Problem
 from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from ..utils import StandardResultsSetPagination
-from rest_framework.response import Response
 
 
 class SubjectAPIView(ListAPIView):
@@ -53,6 +52,24 @@ class PracticeProblemListAPIVIew(ListAPIView):
         return problems
 
 
+# 페이지네이션 제외 연습 문제 / 지역 필터링, 구상 or 즉답형 필터링
+class AllPracticeProblemListAPIVIew(ListAPIView):
+    serializer_class = ProblemListSerializer
+    permission_classes = [IsAuthenticated]
+    model = Problem
+
+    def get_queryset(self):
+        region = int(self.request.query_params.get('region', '1'))
+        type = self.request.query_params.get('type', 'A')
+        problems = Problem.objects.filter(
+            region_id=region,
+            type=type,
+            problem_set__type='P',
+        )
+        return problems
+
+
+# 랜덤 연습 문제 / 지역 필터링, 구상 or 즉답형 필터링
 class RandomPracticeProblemListAPIVIew(ListAPIView):
     serializer_class = ProblemListSerializer
     permission_classes = [IsAuthenticated]
@@ -68,6 +85,23 @@ class RandomPracticeProblemListAPIVIew(ListAPIView):
             problem_set__type='P',
         )
         return sorted(list(problems), key=lambda x: hash((self.request.user.id, x.id)))
+
+
+# 페이지네이션 제외 랜덤 연습 문제 / 지역 필터링, 구상 or 즉답형 필터링
+class AllRandomPracticeProblemListAPIVIew(ListAPIView):
+    serializer_class = ProblemListSerializer
+    permission_classes = [IsAuthenticated]
+    model = Problem
+
+    def get_queryset(self):
+        region = int(self.request.query_params.get('region', '1'))
+        type = self.request.query_params.get('type', 'A')
+        problems = Problem.objects.filter(
+            region_id=region,
+            type=type,
+            problem_set__type='P',
+        ).order_by('?')
+        return problems
 
 
 # 실전 문제셋 (지역별 필터링)
@@ -109,11 +143,55 @@ class RealProblemListAPIVIew(ListAPIView):
         return problems
 
 
+# 실전 문제 리스트 / 페이지네이션 제외
+class AllRealProblemListAPIVIew(ListAPIView):
+    serializer_class = ProblemListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        problem_set = int(self.request.query_params.get('set'))
+        instance = ProblemSet.objects.get(id=problem_set)
+        problems = Problem.objects.filter(
+            problem_set=instance
+        ).prefetch_related('problem_set')
+        return problems
+
+
 # 여기부터 오답노트 리스트
 class ScrappedProblemListAPIView(ListAPIView):
     serializer_class = ProblemListSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = StandardResultsSetPagination
+    model = Problem
+
+    def get_queryset(self):
+        big_subject_id = int(self.request.query_params.get('big_subject', '0'))
+        small_subject_id = int(self.request.query_params.get('small_subject', '0'))
+        problems = Problem.objects.all().prefetch_related('scrapped_users', 'small_subject', 'big_subject')
+        if big_subject_id == 0:
+            problem_list = problems.filter(
+                scrapped_users__in=[self.request.user.id]
+            )
+            return problem_list
+        else:
+            if small_subject_id == 0:
+                problem_list = problems.filter(
+                    scrapped_users__in=[self.request.user.id],
+                    big_subject_id=big_subject_id
+                )
+                return problem_list
+            else:
+                problem_list = problems.filter(
+                    scrapped_users__in=[self.request.user.id],
+                    small_subject__in=[small_subject_id]
+                )
+                return problem_list
+
+
+# 여기부터 오답노트 리스트 / 페이지네이션 제외
+class AllScrappedProblemListAPIView(ListAPIView):
+    serializer_class = ProblemListSerializer
+    permission_classes = [IsAuthenticated]
     model = Problem
 
     def get_queryset(self):
