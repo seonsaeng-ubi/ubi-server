0e01f1890ccddc99b4c970b54e75825c9518d71b
no-gari <sofaissofa@icloud.com>
2025-10-29 21:23:56 +0900

    안드 버전 변경용

--- api/stats/models.py
+++ api/stats/models.py
@@ -18,7 +18,7 @@ class AndroidSetting(models.Model):
     is_blocked = models.BooleanField(default=False, verbose_name='블락 상태')
 
     class Meta:
-        verbose_name = '버전'
+        verbose_name = '안드로이드 버전'
         verbose_name_plural = verbose_name
 
     def __str__(self):
